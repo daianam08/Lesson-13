56cccb72475c7a4cfbc7dc4e0106161e5fb8ea2f
DaianaM <daiana.m.0708@gmail.com>
2023-07-14 09:21:52 -0700

    Initial Commit

---
+++ Lesson 13/Lesson_13App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_13App.swift
+//  Lesson 13
+//
+//  Created by scholar on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_13App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
